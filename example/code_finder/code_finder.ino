3ac5c9caa09953b9d334b11a5ff7ae5f97421691
Tomas kovacik <nail@nodomain.sk>
2018-12-23 17:50:13 +0100

    examples for this library

--- example/code_finder/code_finder.ino
+++ example/code_finder/code_finder.ino
@@ -35,29 +35,13 @@ VOL-:    0x41E800FF
 code is always 0x41 0xE8 X 0xFF-X
 
 */
-
-
-//UP: 0xD0
-#define UP 0xD0
-//0,1,0,0,0,0,0,1,1,1,1,0,1,0,0,0,1,1,0,1,0,0,0,0,0,0,1,0,1,1,1,1
-//DOWN: 0x50
-#define DOWN 0x50
-//0,1,0,0,0,0,0,1,1,1,1,0,1,0,0,0,0,1,0,1,0,0,0,0,1,0,1,0,1,1,1,1
-//LEFT: 0x40
-#define LEFT 0x40
-//0,1,0,0,0,0,0,1,1,1,1,0,1,0,0,0,0,1,0,0,0,0,0,0,1,0,1,1,1,1,1,1
-//RIGHT: 0xC0
-#define RIGHT 0xC0
-//0,1,0,0,0,0,0,1,1,1,1,0,1,0,0,0,1,1,0,0,0,0,0,0,0,0,1,1,1,1,1,1
-//VOL+: 0x80
-//0,1,0,0,0,0,0,1,1,1,1,0,1,0,0,0,1,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1
-//VOL-: 0x00
-//0,1,0,0,0,0,0,1,1,1,1,0,1,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1
+#include "VAGRadioRemote.h"
 #include <LiquidCrystal.h>
+#define REMOTE_PIN 2
 
 //lcd pins
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
-
+VAGRadioRemote remote(REMOTE_PIN);
 
 int adc_key_val[5] ={
   30, 150, 360, 535, 760 };
@@ -66,11 +50,10 @@ int adc_key_in;
 int key=-1;
 int oldkey=-1;
 
-int h=0x00;
+uint8_t h=0x00;
 
 void setup() {
-  pinMode(2,OUTPUT);
-  digitalWrite(2,HIGH);
+  remote.begin();
   lcd.begin(16,2);
   lcd.home();
   lcd.clear();
@@ -94,17 +77,17 @@ void loop() {
       case 0:
         // righ - increment and send
         h++;
-        sendByte(h);
+        remote.send(h);
         break;
       case 1:
-        // up - increment but dont send
+        //increment but dont send
         h++;
           lcd.clear();
   lcd.home();
   lcd.print(h,HEX);
         break;
       case 2:
-        // down - decrement bu dont send
+        //decrement but dont send
         h--; 
   lcd.clear();
   lcd.home();
@@ -114,11 +97,11 @@ void loop() {
         // left - decrement and send
         if(h>0)
         h--; 
-        sendByte(h);
+        remote.send(h);
         break;
       case 4:
         // select - repeat sending of last number
-        sendByte(h);
+        remote.send(h);
         break;
       }
 
@@ -126,42 +109,7 @@ void loop() {
     }
   }
  
-void sendByte(int cislo) {
-  lcd.clear();
-  lcd.home();
-  lcd.print(cislo,HEX);
-  int c_cislo=0xFF-cislo;
-  int cmdStart[32]={0,1,0,0,0,0,0,1,1,1,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-  for (int i = 23;i>=16;i-- ){
-          if (cislo%2  != 0) cmdStart[i]=1;
-          cislo = cislo/2;
-  }
 
-  for (int i = 31;i>=24;i-- ){
-          if (c_cislo%2  != 0) cmdStart[i]=1;
-          c_cislo = c_cislo/2;
-  }
-  digitalWrite(2,LOW);
-  delay(9);
-  digitalWrite(2,HIGH);
-  delayMicroseconds(4500);
-  for (int i=0;i<32;i++){
-    Serial.print(cmdStart[i]);
-    Serial.print(",");
-    digitalWrite(2,LOW);
-    delayMicroseconds(600);
-    digitalWrite(2,HIGH);
-    if (cmdStart[i]==0)
-      delayMicroseconds(600);
-    if (cmdStart[i]==1)
-      delayMicroseconds(1700);
-  }
-  Serial.println("");
-  digitalWrite(2,LOW);
-  delayMicroseconds(600);
-  digitalWrite(2,HIGH);
-
-}
 // Convert ADC value to key number
 int get_key(unsigned int input)
 {
--- example/emulator/emulator.ino
+++ example/emulator/emulator.ino
@@ -95,31 +95,13 @@ discovered codes on my audi concert I unit:
 0xE4 	seek up
 0xE6 	seek up
 */
-
-
-//UP: 0xD0
-#define UP 0xD0
-//0,1,0,0,0,0,0,1,1,1,1,0,1,0,0,0,1,1,0,1,0,0,0,0,0,0,1,0,1,1,1,1
-//DOWN: 0x50
-#define DOWN 0x50
-//0,1,0,0,0,0,0,1,1,1,1,0,1,0,0,0,0,1,0,1,0,0,0,0,1,0,1,0,1,1,1,1
-//LEFT: 0x40
-#define LEFT 0x40
-//0,1,0,0,0,0,0,1,1,1,1,0,1,0,0,0,0,1,0,0,0,0,0,0,1,0,1,1,1,1,1,1
-//RIGHT: 0xC0
-#define RIGHT 0xC0
-//0,1,0,0,0,0,0,1,1,1,1,0,1,0,0,0,1,1,0,0,0,0,0,0,0,0,1,1,1,1,1,1
-//VOL+: 0x80
-//0,1,0,0,0,0,0,1,1,1,1,0,1,0,0,0,1,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1
-//VOL-: 0x00
-//0,1,0,0,0,0,0,1,1,1,1,0,1,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1
-#define MODE 0xA0
-
+#include "VAGRadioRemote.h"
 #include <LiquidCrystal.h>
+#define REMOTE_PIN 2
 
 //lcd pins
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
-
+VAGRadioRemote remote(REMOTE_PIN);
 
 int adc_key_val[5] ={
   30, 150, 360, 535, 760 };
@@ -131,8 +113,7 @@ int oldkey=-1;
 int h=0x00;
 
 void setup() {
-  pinMode(2,OUTPUT);
-  digitalWrite(2,HIGH);
+  remote.begin();
   lcd.begin(16,2);
   lcd.home();
   lcd.clear();
@@ -155,81 +136,45 @@ void loop() {
       {
       case 0:
         // righ
-        sendByte(RIGHT);
   lcd.clear();
   lcd.home();
   lcd.print("NEXT CD/station");
+        remote.right();
         break;
       case 1:
         // up
   lcd.clear();
   lcd.home();
   lcd.print("NEXT track/FF");
-        sendByte(UP);
+        remote.up();
         break;
       case 2:
         // down
   lcd.clear();
   lcd.home();
   lcd.print("PREV track/FR");
-        sendByte(DOWN);
+        remote.down();
         break;
       case 3:
         // left
   lcd.clear();
   lcd.home();
   lcd.print("PREV CD/station");
-        sendByte(LEFT);
+        remote.left();
         break;
       case 4:
         // select
   lcd.clear();
   lcd.home();
   lcd.print("MODE");
-        sendByte(MODE);
+        remote.mode();
         break;
       }
 
      }
     }
   }
-  
-void sendByte(int cislo) {
-  lcd.clear();
-  lcd.setCursor(0, 1);
-  lcd.print(cislo,HEX);
-  int c_cislo=0xFF-cislo;
-  int cmdStart[32]={0,1,0,0,0,0,0,1,1,1,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-  for (int i = 23;i>=16;i-- ){
-          if (cislo%2  != 0) cmdStart[i]=1;
-          cislo = cislo/2;
-  }
-
-  for (int i = 31;i>=24;i-- ){
-          if (c_cislo%2  != 0) cmdStart[i]=1;
-          c_cislo = c_cislo/2;
-  }
-  digitalWrite(2,LOW);
-  delay(9);
-  digitalWrite(2,HIGH);
-  delayMicroseconds(4500);
-  for (int i=0;i<32;i++){
-    Serial.print(cmdStart[i]);
-    Serial.print(",");
-    digitalWrite(2,LOW);
-    delayMicroseconds(600);
-    digitalWrite(2,HIGH);
-    if (cmdStart[i]==0)
-      delayMicroseconds(600);
-    if (cmdStart[i]==1)
-      delayMicroseconds(1700);
-  }
-  Serial.println();
-  digitalWrite(2,LOW);
-  delayMicroseconds(600);
-  digitalWrite(2,HIGH);
-
-}
+ 
 // Convert ADC value to key number
 int get_key(unsigned int input)
 {
