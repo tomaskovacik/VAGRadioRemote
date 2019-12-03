621f8aa1972128712c4d645ed35f24f3f58c6f23
Tomas kovacik <nail@nodomain.sk>
2019-12-03 12:17:53 +0100

    typo

--- src/VAGRadioRemote.cpp
+++ src/VAGRadioRemote.cpp
@@ -105,8 +105,16 @@ telephone option:
 
 
 */
+
 #include "VAGRadioRemote.h"
 #include <Arduino.h>
+
+//#define USE_TIMER0
+//#define USE_TIMER1
+//#define USE_TIMER2
+//#define USE_TIMER3
+//#define USE_TIMER4
+#define USE_TIMER5
 
 volatile static uint8_t data[2];
 volatile static uint8_t sendPtr=0;// 67 is stop => 9000ms,4500ms,4x8x2 + stopbit
@@ -144,6 +152,90 @@ if (inpin != NULL){
 */
 VAGRadioRemote::~VAGRadioRemote(){}
 
+#ifdef USE_TIMER0
+void VAGRadioRemote::setTimer(void){
+  // @ 0.5us ticks
+  TCCR0A = 0x00; // Normal port operation, OC0 disconnected
+  TCCR0B = 0x00; // Normal port operation, OC0 disconnected
+  TCCR0A |= _BV(WGM01); // CTC mode
+  TCCR0B |= _BV(CS01);// prescaler = 8 -> 1 timer clock tick is 0.5us long @ 16Mhz
+  OCR0A = 50;//run compare rutine every 5us, 0.5x10
+  TCNT0 = 0;
+  TIMSK0 |= _BV(OCIE0A); // enable output compare interrupt A on timer0
+}
+#define __TIMERX_COMPA_vect TIMER0_COMPA_vect
+#endif
+
+#ifdef USE_TIMER1
+void VAGRadioRemote::setTimer(void){
+  // @ 0.5us ticks
+  TCCR1A = 0x00; // Normal port operation, OC0 disconnected
+  TCCR1B = 0x00; // Normal port operation, OC0 disconnected
+  TCCR1B |= _BV(WGM12); // CTC mode
+  TCCR1B |= _BV(CS11);// prescaler = 8 -> 1 timer clock tick is 0.5us long @ 16Mhz
+  OCR1A = 50;//run compare rutine every 5us, 0.5x10
+  TCNT1 = 0;
+  TIMSK1 |= _BV(OCIE1A); // enable output compare interrupt A on timer0
+}
+#define __TIMERX_COMPA_vect TIMER1_COMPA_vect
+#endif
+
+#ifdef USE_TIMER2
+void VAGRadioRemote::setTimer(void){
+  // @ 0.5us ticks
+  TCCR2A = 0x00; // Normal port operation, OC0 disconnected
+  TCCR2B = 0x00; // Normal port operation, OC0 disconnected
+  TCCR2A |= _BV(WGM21); // CTC mode
+  TCCR2B |= _BV(CS21);// prescaler = 8 -> 1 timer clock tick is 0.5us long @ 16Mhz
+  OCR2A = 50;//run compare rutine every 5us, 0.5x10
+  TCNT2 = 0;
+  TIMSK2 |= _BV(OCIE2A); // enable output compare interrupt A on timer0
+}
+#define __TIMERX_COMPA_vect TIMER2_COMPA_vect
+#endif
+
+#ifdef USE_TIMER3
+void VAGRadioRemote::setTimer(void){
+  // @ 0.5us ticks
+  TCCR3A = 0x00; // Normal port operation, OC0 disconnected
+  TCCR3B = 0x00; // Normal port operation, OC0 disconnected
+  TCCR3B |= _BV(WGM32); // CTC mode
+  TCCR3B |= _BV(CS31);// prescaler = 8 -> 1 timer clock tick is 0.5us long @ 16Mhz
+  OCR3A = 50;//run compare rutine every 5us, 0.5x10
+  TCNT3 = 0;
+  TIMSK3 |= _BV(OCIE3A); // enable output compare interrupt A on timer0
+}
+#define __TIMERX_COMPA_vect TIMER3_COMPA_vect
+#endif
+
+#ifdef USE_TIMER4
+void VAGRadioRemote::setTimer(void){
+  // @ 0.5us ticks
+  TCCR4A = 0x00; // Normal port operation, OC0 disconnected
+  TCCR4B = 0x00; // Normal port operation, OC0 disconnected
+  TCCR4B |= _BV(WGM42); // CTC mode
+  TCCR4B |= _BV(CS41);// prescaler = 8 -> 1 timer clock tick is 0.5us long @ 16Mhz
+  OCR4A = 50;//run compare rutine every 5us, 0.5x10
+  TCNT4 = 0;
+  TIMSK4 |= _BV(OCIE4A); // enable output compare interrupt A on timer0
+}
+#define __TIMERX_COMPA_vect TIMER4_COMPA_vect
+#endif
+
+#ifdef USE_TIMER5
+void VAGRadioRemote::setTimer(void){
+  // @ 0.5us ticks
+  TCCR5A = 0x00; // Normal port operation, OC0 disconnected
+  TCCR5B = 0x00; // Normal port operation, OC0 disconnected
+  TCCR5B |= _BV(WGM52); // CTC mode
+  TCCR5B |= _BV(CS51);// prescaler = 8 -> 1 timer clock tick is 0.5us long @ 16Mhz
+  OCR5A = 50;//run compare rutine every 5us, 0.5x10
+  TCNT5 = 0;
+  TIMSK5 |= _BV(OCIE5A); // enable output compare interrupt A on timer0
+}
+#define __TIMERX_COMPA_vect TIMER5_COMPA_vect
+#endif
+
 void VAGRadioRemote::begin()
 {
 
@@ -156,18 +248,10 @@ void VAGRadioRemote::begin()
 	pinMode(_inpin, INPUT_PULLUP);
 	attachInterrupt(digitalPinToInterrupt(_inpin), &VAGRadioRemote::remoteInGoingLow, FALLING);
 #endif
-
-	TCCR2A = TCCR2B = 0; // reset
-	TCCR2A |= _BV(WGM21); //CTC mode ,WGM20 and WGM22 are cleared in prev. step
-	TCCR2B |= _BV(CS21);
-	TCCR2B |= _BV(CS20); // prescaler = 32 -> 1 timer clock tick is 2us long
-	OCR2A = 50; //compare run each 10us (5x2us)
-	TCNT2 = 0; //just to be sure
-	TIMSK2 |= _BV(OCIE2A); // enable timer 2 interrupts
-
+	setTimer();
 }
 
-ISR(TIMER2_COMPA_vect)
+ISR(__TIMERX_COMPA_vect)
 {
 #ifdef _INPUT
 
