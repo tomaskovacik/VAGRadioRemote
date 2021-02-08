cdf767ce729000bb6e309f8ffc714306b133ae1c
Tomas kovacik <nail@nodomain.sk>
2021-02-08 19:03:33 +0100

    support for long pres for up/down, user contributed

--- example/code_finder/code_finder.ino
+++ example/code_finder/code_finder.ino
@@ -1,124 +1,151 @@
 /*
 
-license: GPL 
+  license: GPL
 
-code finder for audi radios (probably also for vw, skoda, seat)
-using ebay LCD shield(2x16) with 5 buttons on A0 
-output (remote signal) is on pin 2
+  code finder for audi radios (probably also for vw, skoda, seat)
+  using ebay LCD shield(2x16) with 5 buttons on A0
+  output (remote signal) is on pin 2
 
-more info:
-http://kovo-blog.blogspot.sk/2013/10/remote-control-audi-radio.html
+  more info:
+  http://kovo-blog.blogspot.sk/2013/10/remote-control-audi-radio.html
 
-5V logic
-start bit:
- 9ms LOW
-4.55ms HIGH
+  5V logic
+  start bit:
+  9ms LOW
+  4.55ms HIGH
 
- logic 1:
-~600us LOW ~1700us HIGH
+  logic 1:
+  ~600us LOW ~1700us HIGH
 
-logic 0:
-~600us LOW ~600us HIGH
+  logic 0:
+  ~600us LOW ~600us HIGH
 
-stop bit:
-~600us LOW
+  stop bit:
+  ~600us LOW
 
-I have MFSW without telephone option, so I read codes just for radio:
+  I have MFSW without telephone option, so I read codes just for radio:
 
-UP:       0x41E8D02F
-DOWN: 0x41E850AF
-LEFT:   0x41E840BF
-RIGHT: 0x41E8C03F
-VOL+:  0x41E8807F
-VOL-:    0x41E800FF
+  UP:       0x41E8D02F
+  DOWN: 0x41E850AF
+  LEFT:   0x41E840BF
+  RIGHT: 0x41E8C03F
+  VOL+:  0x41E8807F
+  VOL-:    0x41E800FF
 
-code is always 0x41 0xE8 X 0xFF-X
+  code is always 0x41 0xE8 X 0xFF-X
 
 */
 #include "VAGRadioRemote.h"
+
 #include <LiquidCrystal.h>
+
 #define REMOTE_PIN 2
 
 //lcd pins
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 VAGRadioRemote remote(REMOTE_PIN);
 
-unsigned int adc_key_val[5] ={
-  30, 150, 360, 535, 760 };
-int NUM_KEYS = 5;
-int adc_key_in;
-int key=-1;
-int oldkey=-1;
+unsigned int adc_key_val[5] = {
+    30,
+    150,
+    360,
+    535,
+    760
+};
 
-uint8_t h=0x00;
+#define NUM_KEYS 5
+int key = -1;
+int oldkey = -1;
+uint8_t h = 0x00;
+int get_key();
+void _switch(int key);
+void lcd_print_hex(int key, bool is_send = false);
 
 void setup() {
-  remote.begin();
-  lcd.begin(16,2);
-  lcd.home();
-  lcd.clear();
-  Serial.begin(9600);
+    remote.begin();
+    lcd.begin(16, 2);
+    lcd.home();
+    lcd.clear();
+    lcd.print("Code-Finder v0.2");
 }
 
 void loop() {
-
-  adc_key_in = analogRead(0);    // read the value from the sensor
-  key = get_key(adc_key_in);     // convert into key press
-  if (key != oldkey)             // if keypress is detected
-  {
-    delay(50);          // wait for debounce time
-    adc_key_in = analogRead(0);    // read the value from the sensor
-    key = get_key(adc_key_in);                  // convert into key press
-    if (key != oldkey)
+    key = get_key(); // convert into key press
+    if (key != oldkey) // if keypress is detected
     {
-      oldkey = key;
-      switch (key)
-      {
-      case 0:
-        // righ - increment and send
-        h++;
-        remote.send(h);
-        break;
-      case 1:
-        //increment but dont send
-        h++;
-        break;
-      case 2:
-        //decrement but dont send
-        h--; 
-        break;
-      case 3:
-        // left - decrement and send
-        if(h>0)
-        h--; 
-        remote.send(h);
-        break;
-      case 4:
-        // select - repeat sending of last number
-        remote.send(h);
-        break;
-      }
-  lcd.clear();
-  lcd.home();
-  lcd.print(h,HEX);
-
-     }
+        delay(50); // wait for debounce time
+        key = get_key(); // convert into key press
+        if (key != oldkey) {
+            oldkey = key;
+            _switch(key); //just to be sure
+            if (key == 1 || key == 2) { // Long press support only for 'up' and 'down'
+                delay(200); // wait for long press time
+                while (oldkey == get_key() && oldkey != -1) { //if still pressed, send command
+                    _switch(key);
+                    delay(50);
+                }
+            }
+        }
     }
-  }
- 
+}
+
+void _switch(int key) {
+    switch (key) {
+        case 0:
+            // righ - increment and send
+            h++;
+            remote.send(h);
+            lcd_print_hex(h, true);
+            break;
+        case 1:
+            //increment but dont send
+            h++;
+            lcd_print_hex(h);
+            break;
+        case 2:
+            //decrement but dont send
+            h--;
+            lcd_print_hex(h);
+            break;
+        case 3:
+            // left - decrement and send
+            if (h > 0)
+                h--;
+            remote.send(h);
+            lcd_print_hex(h, true);
+            break;
+        case 4:
+            // select - repeat sending of last number
+            remote.send(h);
+            lcd_print_hex(h, true);
+            break;
+    }
+}
 
 // Convert ADC value to key number
-int get_key(unsigned int input)
-{
-  int k;
-  for (k = 0; k < NUM_KEYS; k++)
-  {
-    if (input < adc_key_val[k])
-    {
-      return k;
+int get_key() {
+    static unsigned int input;
+    input = analogRead(0);
+    int8_t k;
+    for (k = 0; k < NUM_KEYS; k++) {
+        if (input < adc_key_val[k])
+            return k;
     }
-  }
-  if (k >= NUM_KEYS)
-    k = -1;     // No valid key pressed
-  return k;
+    if (k >= NUM_KEYS)
+        return -1; // No valid key pressed
+}
+
+void lcd_print_hex(int key, bool is_send) {
+    lcd.setCursor(0, 1);
+    for (int n = 0; n < 16; n++)
+        lcd.print(" ");
+    lcd.setCursor(0, 1); //sets cursor to second line first row
+
+    char s[20];
+    sprintf(s, "0x%02X", key); // convert to hex with leading 0x
+
+    if (is_send)
+        lcd.print(String(s) + " - send");
+    else
+        lcd.print(s);
 }
